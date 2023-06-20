2f3cb40202382a8932aceb4a871bcca30ab31226
kevincaeyman <kevin.caeyman@gmail.com>
2023-06-20 16:42:18 +0200

    Carousel created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles/index.css";
 import { Header } from "./components/Header";
 import { Navbar } from "./components/Navbar";
+import Carousel from "./components/Carousel"
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Carousel />
     </>
   );
 }
