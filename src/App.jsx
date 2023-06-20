53496e04dc46f092119717b720c1900e3e6e7fd6
Elionor Vendrell <elivendrell@gmail.com>
2023-06-20 15:59:27 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles/index.css";
 import { Header } from "./components/Header";
 import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Footer />
     </>
   );
 }
