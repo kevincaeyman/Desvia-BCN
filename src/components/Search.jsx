6b5baaccd4bc2a37126836565e3371f024c3081c
kevincaeyman <kevin.caeyman@gmail.com>
2023-06-20 23:20:52 +0200

    Responsive design

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/search.css"
 
 export const Search = () => {
   return (
-    <div>
+    <div className="search">
       <h2>Busca lugares alternativos</h2>
       <p className='description'>
         Desvíate y encuentra nuevos puntos de interés en la ciudad de Barcelona
