90e6185ec82a9a7e79ff9052c4fb8074fb5e34f3
Elionor Vendrell <elivendrell@gmail.com>
2023-06-20 23:38:16 +0200

    changes

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import "../styles/information.css"
+import "../styles/information.css";
 
 export const Information = () => {
   return (
-
     <div id='information'>
-
-    <div className="information">
-
-      <h2>Information</h2>
-      <p className='description'>
-        Explore the tourist reality of Barcelona through our informative charts:
-        precise and revealing data that will show you the pulse of tourism in
-        the Catalan capital.
-      </p>
+      <div className='information'>
+        <h2>Information</h2>
+        <p className='description'>
+          Explore the tourist reality of Barcelona through our informative
+          charts: precise and revealing data that will show you the pulse of
+          tourism in the Catalan capital.
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import "../styles/search.css"
+import "../styles/search.css";
 
 export const Search = () => {
   return (
-
     <div id='discover'>
-      <h2>Discover alternative places</h2>
+      <div className='search'>
+        <h2>Discover alternative places</h2>
 
-    <div className="search">
-      <h2>Busca lugares alternativos</h2>
-
-      <p className='description'>
-        Unveil hidden gems as you navigate through Barcelona's districts.
-        Explore our curated selection of non-touristy locations, allowing you to
-        immerse yourself in authentic local experiences. Find
-        off-the-beaten-path spots and unlock the true essence of the city.
-      </p>
+        <p className='description'>
+          Unveil hidden gems as you navigate through Barcelona's districts.
+          Explore our curated selection of non-touristy locations, allowing you
+          to immerse yourself in authentic local experiences. Find
+          off-the-beaten-path spots and unlock the true essence of the city.
+        </p>
+      </div>
     </div>
   );
 };
