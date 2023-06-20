fbf854040e9a675e23908290470c21bb79aa14a0
Elionor Vendrell <elivendrell@gmail.com>
2023-06-20 15:51:38 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,25 +5,21 @@ export const Navbar = () => {
     <div>
       <div className='navbar bg-base-100'>
         <div className='flex-1'>
-          <a className='btn btn-ghost normal-case text-xl'>daisyUI</a>
+          <p className='btn btn-ghost normal-case text-xl logo'>Des/vía</p>
         </div>
         <div className='flex-none'>
           <ul className='menu menu-horizontal px-1'>
             <li>
-              <a>Link</a>
+              <a>Information</a>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className='p-2 bg-base-100'>
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
+              <a>Discover</a>
+            </li>
+            <li>
+              <a>Mobility</a>
+            </li>
+            <li>
+              <a>About us</a>
             </li>
           </ul>
         </div>
