46fd0d2b405334d05aaa63c4af1f9b18695a7c68
Elionor Vendrell <elivendrell@gmail.com>
2023-06-20 18:51:58 +0200

    header3d

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,19 @@ import React from "react";
 import "./styles/index.css";
 import { Header } from "./components/Header";
 
-import { FiberContainer } from "./components/FiberContainer";
+
 
 import { Navbar } from "./components/Navbar";
 import Carousel from "./components/Carousel";
 import { Footer } from "./components/Footer";
 import { Information } from "./components/Information";
-import BarChart from "./components/BarChart";
 import { Search } from "./components/Search";
 import Filters from "./components/Filters";
 import { MotoSharing } from "./components/MotoSharing";
 
-
 function App() {
   return (
     <>
-
-      <Header/>
-      <div className="bg-model">
-        <FiberContainer />
-      </div>
-      <div className="test">
-
-      </div>
-      <div className="test1">
-
-      </div>
-
       <Navbar />
       <Header />
       <Information />
@@ -37,7 +23,6 @@ function App() {
       <Filters />
       <MotoSharing />
       <Footer />
-
     </>
   );
 }
