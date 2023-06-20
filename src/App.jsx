57c928a9aec81878633758ec16f60593c307f4b0
Elionor Vendrell <elivendrell@gmail.com>
2023-06-20 13:39:34 +0200

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./styles/index.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <h2>Hola</h2>
       <p className='description'>
         Desvíate y encuentra nuevos puntos de interés en la ciudad de Barcelona
